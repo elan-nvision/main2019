374c64b2e68530aab71282cd85d78eb5f33c8fe3
CrazyByDefault <es16btech11025@iith.ac.in>
2019-01-20 17:32:00 +0530

    slow down canva.js based on width

--- public/js/canva.js
+++ public/js/canva.js
@@ -7,7 +7,7 @@ var w = window.innerWidth,
     time,
     count,
     size = 7,
-    speed = 20,
+    speed = 10 + 0.01*window.innerWidth,
     parts = new Array,
     colors = ['#16A085', '#2ECC71', '#27AE60', '#3498DB','#2980B9','#9B59B6','#8E44AD','#34495E',
       '#F1C40F','#F39C12','#E67E22','#D35400','#E74C3C','#C0392B','#ECF0F1','#BDC3C7','#95A5A6','#7F8C8D'];
